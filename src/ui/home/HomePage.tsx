0ef42979bba790260f9323c0b0410eb33a02421b
ralph <nobody@localhost>
2023-10-09 10:13:58 -0700

    add basic home page

--- src/ui/home/HomePage.tsx
+++ src/ui/home/HomePage.tsx
@@ -1,12 +1,25 @@
 // Home Page
-
 import "./HomePage.scss";
 
 /**
  * Renders the home page.
  * @returns 
  */
-export const HomePage = ()=><div className="page-home">
-    Home Page.
-</div>;
+export const HomePage = ()=>{
+    return <div className="page home">
+        <div className="right form">
+            <div className="title">Twit2</div>
+            <div className="subtitle">The next generation of social media</div>
+            <div className="separator"></div>
+            <div className="buttons">
+                <button>Log In</button>
+                <button>Create Account</button>
+            </div>
+            <div className="links">
+                <a href="/tos">Terms of Service</a> | 
+                <a href="/privacy">Privacy Policy</a>
+            </div>
+        </div>
+    </div>;
+}
 
