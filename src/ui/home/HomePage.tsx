b460badd9f796ee354f3fad08f42bdbdf798da9a
ralph <nobody@localhost>
2023-10-09 09:55:43 -0700

    Create structure

---
+++ src/ui/home/HomePage.tsx
@@ -0,0 +1,12 @@
+// Home Page
+
+import "./HomePage.scss";
+
+/**
+ * Renders the home page.
+ * @returns 
+ */
+export const HomePage = ()=><div className="page-home">
+    Home Page.
+</div>;
+
---
+++ src/ui/logon/LoginPage.tsx
@@ -0,0 +1,10 @@
+// Logon Page
+
+/**
+ * Renders the login page.
+ * @returns 
+ */
+export const LoginPage = ()=><div className="page-login">
+    Login Page.
+</div>;
+
