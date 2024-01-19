f27fa096fc545c400b48c6d8dc88376792584343
GustavoFringGaming <113339801+GustavoFringGaming@users.noreply.github.com>
2024-01-18 21:56:23 -0500

    fix feed ui

--- src/ui/main/sublayouts/FeedView.tsx
+++ src/ui/main/sublayouts/FeedView.tsx
@@ -1,5 +1,7 @@
 import { TitleHeader } from "../../../components/layout/TitleHeader";
+import { PostBox, PostBoxMode } from "../../../components/main/post/PostBox";
 
 export const FeedView = ()=><div className="view feed">
     <TitleHeader title="My Feed"/>
+    <PostBox mode={PostBoxMode.FeedPosts} target={""}/>
 </div>;
